bdf429713b1a242c1936b3cf58021e2009615266
Uddhav <uddhavkrishna151@gmail.com>
2026-04-03 22:55:38 +0530

    Added 3Sum

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,4 +1,6 @@
-class Solution {
+import java.util.HashMap;
+
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> visited = new HashMap<>();
         for (int i=0;i<nums.length;i++) {
